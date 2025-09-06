94004848074b7060f1504572f446c139a706996a
Andreas Sousa <devandreas96@gmail.com>
2025-09-06 20:47:29 -0300

    `Update dependencies to vite 7.1.4 and refactor button functionality to use React hooks`

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 
-import {Reset, Decrease, Increase} from "./buttons"
+import { useState } from "react";
 import {ReactLogo} from "./ReactLogo"
 
-function App() {
-  let counter = document.querySelector("#counter")
+function App() {  
+  const [counter, setCounter] = useState(0)
+  
+  function diminuir(){
+    setCounter(prevs => prevs - 1)
+    if(counter <= 0 ){
+      contador.style.color = 'rgb(255, 114, 59)'
+    } else if (counter == 1){
+      contador.style.color = 'white'
+
+    }
+  }
+
+  function aumentar(){
+    setCounter(prevs => prevs + 1)
+    if(counter >= 0 ){
+      contador.style.color = 'rgb(59, 248, 255)'
+    } else if (counter == -1){
+      contador.style.color = 'white'
+
+    }
+  }
+
+  function resetar(){
+    setCounter(0)
+    contador.style.color = 'white'
+  }
 
   return (
     <>
       <ReactLogo />
-      <h1>Contador</h1>
-      <span id="counter">0</span>
+      <h1>CONTADOR</h1>
+      <span id="contador">{counter}</span>
       <div className="buttons">
-        <Decrease />
-        <Reset />
-        <Increase />
+        <button onClick={diminuir}>Diminuir</button>
+        <button onClick={resetar}>Resetar</button>
+        <button onClick={aumentar}>Aumentar</button>
       </div>
     </>
   )
