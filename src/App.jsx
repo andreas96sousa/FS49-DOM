16f27251fe5cf08fe17a84b39ecdf47b4fc91bf6
Andreas Sousa <devandreas96@gmail.com>
2025-09-06 20:56:45 -0300

    `Refactor App component: update state management and styling`

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,44 @@
 import { useState } from "react";
 import {ReactLogo} from "./ReactLogo"
 
-function App() {  
+function App() {
   const [counter, setCounter] = useState(0)
+  const [color, setColor] = useState('white')
   
   function diminuir(){
-    setCounter(prevs => prevs - 1)
-    if(counter <= 0 ){
-      contador.style.color = 'rgb(255, 114, 59)'
-    } else if (counter == 1){
-      contador.style.color = 'white'
-
+    const newCounter = counter - 1;
+    setCounter(newCounter);
+    if(newCounter > 0){
+      setColor('rgb(59, 248, 255)');
+    } else if(newCounter < 0){
+      setColor('rgb(255, 114, 59)');
+    } else {
+      setColor('white');
     }
   }
 
   function aumentar(){
-    setCounter(prevs => prevs + 1)
-    if(counter >= 0 ){
-      contador.style.color = 'rgb(59, 248, 255)'
-    } else if (counter == -1){
-      contador.style.color = 'white'
-
+    const newCounter = counter + 1;
+    setCounter(newCounter);
+    if(newCounter > 0){
+      setColor('rgb(59, 248, 255)');
+    } else if(newCounter < 0){
+      setColor('rgb(255, 114, 59)');
+    } else {
+      setColor('white');
     }
   }
 
   function resetar(){
-    setCounter(0)
-    contador.style.color = 'white'
+    setCounter(0);
+    setColor('white');
   }
 
   return (
     <>
       <ReactLogo />
       <h1>CONTADOR</h1>
-      <span id="contador">{counter}</span>
+      <span id="contador" style={{color}}>{counter}</span>
       <div className="buttons">
         <button onClick={diminuir}>Diminuir</button>
         <button onClick={resetar}>Resetar</button>
